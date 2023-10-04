bacc52b666ff8cbd61e4adc46cbf7194ec9fd6be
Abhay Dhaka <abhay.dhaka@nagarro.com>
2023-10-04 15:10:01 +0530

    Jenkins Assignment

---
+++ src/main/java/JenkinsAssignment/SampleTestCases/App.java
@@ -0,0 +1,13 @@
+package JenkinsAssignment.SampleTestCases;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/JenkinsAssignment/SampleTestCases/AppTest.java
@@ -0,0 +1,49 @@
+package JenkinsAssignment.SampleTestCases;
+
+ 
+
+ 
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+ 
+
+ 
+
+public class AppTest 
+{
+	@Test
+	public void launchBrowser() {
+        // Set the path to the ChromeDriver executable
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\abhaydhaka\\Downloads\\Drivers\\Drivers\\chromedriver.exe");
+
+ 
+
+ 
+
+        // Initialize the ChromeDriver
+        WebDriver driver = new ChromeDriver();
+
+ 
+
+ 
+
+        // Open a website
+        driver.get("https://www.google.com");
+
+
+
+ 
+
+        // Perform your actions here
+
+ 
+
+ 
+
+        // Close the browser
+       driver.quit();
+    }
+}
